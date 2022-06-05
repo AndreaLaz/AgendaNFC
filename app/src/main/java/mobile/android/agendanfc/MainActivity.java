45735bbe04c107751520394a036f5e8dba2b580b
AndreaLaz <106910406+AndreaLaz@users.noreply.github.com>
2022-06-05 14:30:41 +0200

    05/06/22 mainActivity/iniciarsesionActivity /registrarseActivity
    crear iniciarsesionActivity
    crear registrarseActivity
    
    y hacer la parte visial

--- app/src/main/java/mobile/android/agendanfc/MainActivity.java
+++ app/src/main/java/mobile/android/agendanfc/MainActivity.java
@@ -2,7 +2,9 @@ package mobile.android.agendanfc;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void irInicioSesion(View view){
+        Intent i = new Intent(this,IniciarSesionActivity.class);//de donde estamos a donde queremos ir
+        startActivity(i);//para ir de una ventana a otra
+    }
+    public void irRegistro(View view){
+        Intent i = new Intent(this,RegistrarseActivity.class);//de donde estamos a donde queremos ir
+        startActivity(i);//para ir de una ventana a otra
     }
 }
