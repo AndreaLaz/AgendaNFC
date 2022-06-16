5fe75459c2c1292e1126163eb3fab6994d0fc7ce
AndreaLaz <106910406+AndreaLaz@users.noreply.github.com>
2022-06-16 21:00:38 +0200

    Cerrar Sesion- Authentication Firebase

--- app/src/main/java/mobile/android/agendanfc/MainActivity.java
+++ app/src/main/java/mobile/android/agendanfc/MainActivity.java
@@ -5,14 +5,36 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class MainActivity extends AppCompatActivity {
+    FirebaseAuth auth;
+
+    private Button login,registrarse,cerrarSesion;
+    private TextView opciones;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        login = findViewById(R.id.buttonIDiniciarsesion);
+        registrarse = findViewById(R.id.buttonIDregistrarse);
+        cerrarSesion = findViewById(R.id.btoidcerrarsesion);
+        opciones = findViewById(R.id.tvOr);
+
+        auth = FirebaseAuth.getInstance();
+
+        if(auth.getCurrentUser()!=null){
+            login.setVisibility(View.GONE);
+            registrarse.setVisibility(View.GONE);
+            cerrarSesion.setVisibility(View.VISIBLE);
+            opciones.setVisibility(View.GONE);
+        }
 
     }
     public void irInicioSesion(View view){
@@ -22,5 +44,14 @@ public class MainActivity extends AppCompatActivity {
     public void irRegistro(View view){
         Intent i = new Intent(this,RegistrarseActivity.class);//de donde estamos a donde queremos ir
         startActivity(i);//para ir de una ventana a otra
+    }
+    public void cerrarSesion(View view){
+        auth.signOut();
+        if(auth.getCurrentUser() == null){
+            login.setVisibility(View.VISIBLE);
+            registrarse.setVisibility(View.VISIBLE);
+            cerrarSesion.setVisibility(View.GONE);
+            Toast.makeText(this,"Sesion cerrada",Toast.LENGTH_SHORT).show();
+        }
     }
 }
--- app/src/main/java/mobile/android/agendanfc/MenuPrincipalActivity.java
+++ app/src/main/java/mobile/android/agendanfc/MenuPrincipalActivity.java
@@ -2,7 +2,11 @@ package mobile.android.agendanfc;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MenuPrincipalActivity extends AppCompatActivity {
 
@@ -10,5 +14,9 @@ public class MenuPrincipalActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_principal);
+    }
+    public void irInicio(View view){
+        Intent i = new Intent(this,MainActivity.class);
+        startActivity(i);
     }
 }
